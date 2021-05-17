b89aeb20320d7b13c671eedc94d9fbcd9f4d4a48
daqy <541064029@qq.com>
2021-05-17 17:57:24 +0800

    add day3

---
+++ day3/swap.cpp
@@ -0,0 +1,25 @@
+//
+// Created by user on 2021/5/17.
+//
+#include<iostream>
+
+using namespace std;
+
+void swap(int &a, int &b);
+
+int main(int argc, char const *argv[]) {
+    int x1 = 5;
+    int x2 = 7;
+    swap(x1, x2);
+    cout << x1 << " " << x2 << endl;
+    system("pause");
+    return 0;
+}
+
+void swap(int &a, int &b) {
+    int t;
+    t = a;
+    a = b;
+    b = t;
+
+}
